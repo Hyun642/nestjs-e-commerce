3201fffff319a446e340817d063019abf0cfa6ca
Hyun <105111234+Hyun642@users.noreply.github.com>
2025-01-11 00:05:31 +0900

    feat: enforce unique username for user signup

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
@@ -14,6 +18,14 @@ export class UserRepository extends Repository<User> {
       username,
       password,
     });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Existing username');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
