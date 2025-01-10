04eca0dd197dd64d5c7b742af3bca5515626f18e
Hyun <jjjtoy@naver.com>
2025-01-10 14:19:23 +0900

    feat: implement getBoardById API to retrieve board by ID from database

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -4,9 +4,13 @@ import { v4 as uuid } from 'uuid';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { BoardRepository } from './board.repository';
+import { Board } from './board.entity';
 
 @Injectable()
 export class BoardsService {
+  constructor(
+    @InjectRepository(BoardRepository) private boardRepository: BoardRepository,
+  ) {}
   // private boards: Board[] = [];
   // getAllBoards(): Board[] {
   //   return this.boards;
@@ -22,13 +26,13 @@ export class BoardsService {
   //   this.boards.push(board);
   //   return board;
   // }
-  // getBoardById(id: string): Board {
-  //   const found = this.boards.find((board) => board.id === id);
-  //   if (!found) {
-  //     throw new NotFoundException(`can't find Board with id ${id}`);
-  //   }
-  //   return found;
-  // }
+  async getBoardById(id: number): Promise<Board> {
+    const found = await this.boardRepository.findOne({ where: { id } });
+    if (!found) {
+      throw new NotFoundException(`can't find Board with id ${id}`);
+    }
+    return found;
+  }
   // deleteBoard(id: string): void {
   //   const found = this.getBoardById(id);
   //   this.boards = this.boards.filter((board) => board.id !== found.id);
