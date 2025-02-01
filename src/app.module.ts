def681a9fdd302612722262e6630e7b70ab9fbde
Hyun <105111234+Hyun642@users.noreply.github.com>
2025-02-01 21:02:24 +0900

    Reset project to initial state

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
-import { Module, ValidationPipe } from '@nestjs/common';
-import { BoardsModule } from './boards/boards.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeORMConfig } from './configs/typeorm.config';
-import { AuthModule } from './auth/auth.module';
-import { APP_PIPE } from '@nestjs/core';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  providers: [{ provide: APP_PIPE, useClass: ValidationPipe }],
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule, AuthModule],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
-import * as config from 'config';
+import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const serverConfig = config.get('server');
-  const port = serverConfig.port;
-  // app.useGlobalPipes(new ValidationPipe());
-  await app.listen(port);
-  Logger.log(`App running on port ${port}`);
+
+  await app.listen(3000);
+  Logger.log(`App running on port 3000`);
 }
 bootstrap();
