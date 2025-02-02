57078a2e90f7bad026de65be8fe714fd7bd16def
Hyun <105111234+Hyun642@users.noreply.github.com>
2025-02-03 00:04:39 +0900

    feat: apply global pipe for validation

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from './databases/prisma/prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks();
-
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
   Logger.log(`App running on port 3000`);
 }
