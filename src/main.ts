cd2d0a554bb7ce152e9abb7434b6b90263f1191f
Hyun <105111234+Hyun642@users.noreply.github.com>
2025-02-02 23:08:12 +0900

    chore: Add Prisma Client dependency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { PrismaService } from './databases/prisma/prisma.service';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks();
 
   await app.listen(3000);
   Logger.log(`App running on port 3000`);
