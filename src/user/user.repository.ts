71082573f6adefd018c120f90daa70f949d97c0c
Hyun <jjjtoy@naver.com>
2025-02-03 15:10:44 +0900

    feat: apply bcrypt password hashing for secure user signup

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -2,6 +2,7 @@ import { ConflictException, Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/databases/prisma/prisma.service';
 import { SignUpDto } from './dto/signup.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserRepository {
@@ -14,11 +15,15 @@ export class UserRepository {
     if (existingUser) {
       throw new ConflictException('이미 등록된 이메일 입니다.');
     }
+
+    const saltOrRounds = 10;
+    const hashedPassword = await bcrypt.hash(user.password, saltOrRounds);
+
     const res = await this.prisma.user.create({
       data: {
         email: user.email,
         name: user.name,
-        password: user.password,
+        password: hashedPassword,
         phoneNumber: user.phoneNumber,
       },
     });
