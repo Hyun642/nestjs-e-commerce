6c28fd70041102067a048493abd58640f909bdb3
Hyun <jjjtoy@naver.com>
2025-02-12 13:32:33 +0900

    feat: main.ts에 Swagger 설정 추가 및 API 문서 엔드포인트 구축

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { PrismaService } from './databases/prisma/prisma.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('e-commerce')
+    .setDescription('e-commerce API description')
+    .setVersion('1.0')
+    .addTag('e-commerce')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks();
