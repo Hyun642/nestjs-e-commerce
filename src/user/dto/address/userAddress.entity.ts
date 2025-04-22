ec78f2c61aba16192b1a04d2a99c47775505eea6
Hyun <jjjtoy@naver.com>
2025-04-22 14:04:09 +0900

    feat: userAddress entity 생성

---
+++ src/user/dto/address/userAddress.entity.ts
@@ -0,0 +1,33 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsDateString, IsInt, IsOptional, IsString } from 'class-validator';
+
+export class UserAddressEntity {
+  @ApiProperty({ description: '주소 ID', example: 1 })
+  @IsInt()
+  id: number;
+
+  @ApiProperty({ description: '사용자 ID', example: '1#ekw12@3w2' })
+  @IsString()
+  userId: string;
+
+  @ApiProperty({ description: '주소 별칭: 집, 회사', example: '집' })
+  @IsString()
+  name: string;
+
+  @ApiProperty({
+    description: '상세 주소',
+    example: '경기도 수원시 오리로 2-1, 101동 101호',
+  })
+  @IsString()
+  address: string;
+
+  @ApiProperty({
+    description: '삭제 일시',
+    example: '2025-04-21T10:00:00.000Z',
+    required: false,
+  })
+  @IsOptional()
+  @IsDateString()
+  deletedAt: Date;
+}
